8b7488cb3db7c2e122283b7525b4a84449737c89
Terrell Russell <terrellrussell@gmail.com>
2024-02-13 10:21:05 -0500

    first contact

--- docker-testing/run_the_tests.py
+++ docker-testing/run_the_tests.py
@@ -2,6 +2,11 @@ import irods
 
 print('the tests have begun')
 
+from irods.session import iRODSSession
+with iRODSSession(host='localhost', port=1247, user='rods', password='rods', zone='tempZone') as session:
+    coll = session.collections.get("/tempZone/home/rods")
+    print('collection id', coll.id)
+
 print('the tests are complete')
 
 exit(0)
